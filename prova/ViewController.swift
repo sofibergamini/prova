10fcc9736c265d3dfca810123c2c29dd348429ed
Sofia Bergamini <sofia.bergamini@gmail.com>
2022-10-27 21:20:53 +0200

    Initial Commit

---
+++ prova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prova
+//
+//  Created by Sofia Bergamini on 27/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
